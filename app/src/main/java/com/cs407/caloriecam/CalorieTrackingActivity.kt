6a93eb91758221dd63552c29b8e83eae54aca199
Anurag Ghadge <anurag29.ghadge@gmail.com>
2024-12-09 23:21:28 -0600

    Save local changes before pulling

---
+++ app/src/main/java/com/cs407/caloriecam/CalorieTrackingActivity.kt
@@ -0,0 +1,11 @@
+package com.cs407.caloriecam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalorieTrackingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calorie_tracking)
+    }
+}
