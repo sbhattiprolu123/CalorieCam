18d5cba42b444bb818279bf582e1a097199dc4b0
antonyrainchik <83198700+antonyrainchik@users.noreply.github.com>
2024-12-10 21:34:54 -0600

    this took way too long, but I still might be the greatest

--- app/src/main/java/com/cs407/caloriecam/HomePageFragment.kt
+++ app/src/main/java/com/cs407/caloriecam/HomePageFragment.kt
@@ -17,15 +17,21 @@ class HomePageFragment : Fragment(R.layout.calorie_progress) {
         val caloriesTextView: TextView = view.findViewById(R.id.CalorieAmount)
         val caloriesProgressView: ProgressBar = view.findViewById(R.id.progressBar)
         val logButton: Button = view.findViewById(R.id.logButton)
+        val resetButton: Button = view.findViewById(R.id.resetButton)
 
         val prefs = requireContext().getSharedPreferences("MY_PREFS", 0)
         val totalCalories = totalCalories + prefs.getInt("total_calories", 0)
 
         caloriesTextView.text = "$totalCalories/2000"
         caloriesProgressView.progress = (totalCalories*100)/2000
-        Log.d("D",caloriesProgressView.progress.toString())
         logButton.setOnClickListener{
             findNavController().navigate(R.id.action_HomePageFragment_to_MealLoggingFragment)
+        }
+        resetButton.setOnClickListener{
+            prefs.edit().putInt("total_calories",0)
+                .apply()
+            caloriesTextView.text = "0/2000"
+            caloriesProgressView.progress=0
         }
 
     }
